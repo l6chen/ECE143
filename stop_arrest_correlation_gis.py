0a907c04dc0ebae8016bd7b6591638139d8e495e
Raul Pegan <raulpegan1@gmail.com>
2018-12-03 15:46:09 -0800

    transferred to .py, need to turn it into functions etc

---
+++ stop_arrest_correlation_gis.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import geopandas
+
+pd.options.display.max_rows = 10
+
+divisions = geopandas.read_file("zip://./datasets/pd_divisions_datasd.zip")
+beats = geopandas.read_file("zip://./datasets/pd_beats_datasd.zip")
+
+df = pd.read_csv('./datasets/vehicle_stops_2016_datasd_original.csv')
+
+#Clean the values
+stops_raw = df[['arrested','service_area']].dropna()
+arrest_values = ['Y', 'N']
+stops_raw2 = stops_raw.loc[stops_raw['arrested'].isin(arrest_values)]
+stops_raw3 = stops_raw2.loc[stops_raw2['service_area'] != 'Unknown']
+
+# Amount of stops per service area
+stop_count = stops_raw3.groupby('service_area').count()
+
+# Amount of arrests per service area
+arrested_final_count = stops_raw3.loc[stops_raw3['arrested'] == 'Y'].groupby('service_area').count()
+
+# Amount of stops without arrests per service area
+not_arrested_final_count = stops_raw3.loc[stops_raw3['arrested'] == 'N'].groupby('service_area').count()
+
+# Probability of arrest per service area
+probability_of_arrest = (arrested_final_count/stop_count).fillna(0)
+
+# Build the beats datafram to merge and get the divisions
+beats2 = beats[['div', 'serv']]
+# Only care about one appearance of each serv
+grouped = beats2.groupby('serv').first()
+beats3 = grouped[['div']]
+
+# Clean the indeces and the datatypes
+stop_count = stop_count.reset_index()
+probability_of_arrest = probability_of_arrest.reset_index()
+beats3 = beats3.reset_index()
+stop_count['service_area'] = stop_count['service_area'].astype(int)
+probability_of_arrest['service_area'] = probability_of_arrest['service_area'].astype(int)
+beats3['serv'] = beats3['serv'].astype(int)
+
+# Join beats and df
+stopped_by_area = beats3.join(stop_count.set_index('service_area'), on='serv').dropna()
+stopped_by_area[['arrested']] = stopped_by_area[['arrested']].fillna(0).astype(int)
+
+# Join divisions and df
+stopped_by_area = stopped_by_area.groupby('div').sum()['arrested']
+divsums_stop = divisions.join(stopped_by_area, on='div_num')
+
+# Repeat the same process as above, but with probability of arrest
+arrested_prob_by_area = beats3.join(probability_of_arrest.set_index('service_area'), on='serv').dropna()
+arrested_prob_by_area[['arrested']] = arrested_prob_by_area[['arrested']].fillna(0)
+
+arrested_prob_by_area = arrested_prob_by_area.groupby('div').sum()['arrested']
+divsums_arrest = divisions.join(arrested_prob_by_area, on='div_num')
+
+# Plot the results
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
+
+divsums_stop.plot(ax=ax, column='arrested', figsize=(30, 12), cmap='OrRd')
+ax.set_axis_off()
+divsums_stop.apply(lambda x: ax.annotate(s=x.div_name.capitalize(), xy=x.geometry.centroid.coords[0], ha='center'),axis=1)
+
+plt.show()
+
+# Plot the second one
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
+
+divsums_arrest.plot(ax=ax, column='arrested', figsize=(30, 12), cmap='OrRd')
+ax.set_axis_off()
+divsums_arrest.apply(lambda x: ax.annotate(s=x.div_name.capitalize(), xy=x.geometry.centroid.coords[0], ha='center'),axis=1)
+
+plt.show()
