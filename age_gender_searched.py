169683374c8c706e5358353d9d3aadd1bca05de1
Lingfeng Chen <l6chen@eng.ucsd.edu>
2018-12-07 14:55:53 -0800

    put plot codes in function

--- age_gender_searched.py
+++ age_gender_searched.py
@@ -5,9 +5,53 @@ from collections import defaultdict
 from matplotlib.pylab import subplots
 import matplotlib.pyplot as plt
 
+
+cleanedcsv = cleandata('./datasets/vehicle_stops_2016_datasd_original.csv')#clean the dataset and write it in a new csv file
+dataset = read_data(cleanedcsv)#read the cleaned dataset as dataframe
+
+def plot(chart, zoom = 'n'):
+    '''
+    This function is intended to plot three different charts
+    '''
+    if chart == 'stopped':
+        male_stopped_array, female_stopped_array = create_data(mode='stopped')
+        fig,ax=subplots()
+        ax.plot(male_stopped_array[0],male_stopped_array[1],female_stopped_array[0],female_stopped_array[1])
+        ax.legend(('Male','Female'),loc='best',prop={'size':24})
+        plt.xlabel('Age',fontsize=16)
+        plt.ylabel('Stopped Times',fontsize=16)
+        plt.title('Stopped Times VS Age & Gender',fontsize=16)
+        fig.set_size_inches(16, 9)
+        
+    elif chart == 'searched':
+        male_searched_array, female_searched_array = create_data(mode='searched')
+        fig,ax=subplots()
+        ax.plot(male_searched_array[0],male_searched_array[1],female_searched_array[0],female_searched_array[1])
+        ax.legend(('Male','Female'),loc='best',prop={'size':24})
+        plt.xlabel('Age',fontsize=16)
+        plt.ylabel('Searched Times',fontsize=16)
+        plt.title('Searched Times VS Age & Gender',fontsize=16)
+        fig.set_size_inches(16, 9)
+        
+    elif chart == 'ratio':
+        male_ratio_array, female_ratio_array = create_data(mode='ratio')
+        fig,ax=subplots()
+        if zoom == 'y':
+            ax.plot(male_ratio_array[0][1:-1],male_ratio_array[1][1:-1],female_ratio_array[0],female_ratio_array[1])
+        else:
+            ax.plot(male_ratio_array[0],male_ratio_array[1],female_ratio_array[0],female_ratio_array[1])
+        ax.plot(male_ratio_array[0],len(male_ratio_array[0]+1)*[np.mean(male_ratio_array[1])],\
+        female_ratio_array[0],len(female_ratio_array[0]+1)*[np.mean(female_ratio_array[1])],linestyle="--")
+        ax.legend(('Male','Female','Mean of Male','Mean of Female'),loc='best',prop={'size':20})
+        plt.xlabel('Age',fontsize=16)
+        plt.ylabel('Searched Times/Stopped Times',fontsize=16)
+        plt.title('Searched Probability VS Age & Gender',fontsize=16)
+        fig.set_size_inches(16, 9)
+        
+
 def cleandata(ori):
     '''
-    This function is intended to clean the bad data(like "no data/unknow/NaN") for later processing.
+    This function is intended to clean the bad data(like "no data/unknow/NaN") for later processing. We then put the cleaned dataset in a new csv file.
     Inputs: 
     ori: original file name
     cor: the target correlation datasets:['subject_sex','subject_age','searched']
@@ -17,14 +61,14 @@ def cleandata(ori):
     
     assert isinstance(ori,str)
     cleaned = './datasets/gender_age_searched_cleaned.csv'
-    with open(ori, mode='r') as in_file, open(cleaned, mode='w') as out_file:
+    with open(ori, mode='r') as in_file, open(cleaned, mode='w') as out_file: #read a file and create a new file for cleaned data
         csv_reader = csv.DictReader(in_file)
         csv_writer = csv.writer(out_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         line_count = 0
         empty_arrest_rows_count = 0
         for row in csv_reader:
             if line_count == 0:
-                print(f'Column names are {", ".join(row)}')
+                #print(f'Column names are {", ".join(row)}')
                 csv_writer.writerow(row)
                 line_count += 1
             else:
@@ -107,7 +151,8 @@ def create_data(mode='ratio'):
         return male_stopped_array, female_stopped_array
     elif mode == 'searched':
         return male_searched_array, female_searched_array
+
+
     
-cleanedcsv = cleandata('./datasets/vehicle_stops_2016_datasd_original.csv')
-dataset = read_data(cleanedcsv)
+
 
